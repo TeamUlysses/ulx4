f025a8de601e1d53709c1d4ea81c8af8e2d20371
SticklyMan <strippon@gmail.com>
2015-03-13 18:12:00 -0600

    Update make.bat add 'all' choice, which can call multiple tasks.

--- make.bat
+++ make.bat
@@ -2,83 +2,84 @@
 ::Takes one parameter for task.
 @ECHO OFF
 SET choice=%1
+SET tasks=clean, lua, watch, lint, map, test, diagram, doc, all
 
 ::Ask for make task if not specified
 IF /I "%choice%"=="" (
 SET didinput=1
-echo.
+echo/
 echo Valid Tasks are:
-echo    clean, lua, watch, lint, map, test, diagram, doc
-echo.
+echo    %tasks%
+echo/
 SET /P choice="What would you like to make?: "
 )
 
 
 ::Handle task input
 IF /I "%choice%"=="" GOTO INVALID
-IF /I %choice%==clean GOTO clean
-IF /I %choice%==lua GOTO lua
-IF /I %choice%==watch GOTO watch
-IF /I %choice%==lint GOTO lint
-IF /I %choice%==map GOTO map
-IF /I %choice%==test GOTO test
-IF /I %choice%==diagram GOTO diagram
-IF /I %choice%==doc GOTO doc
+IF /I %choice%==clean (CALL :clean & GOTO end)
+IF /I %choice%==lua (CALL :lua & GOTO end)
+IF /I %choice%==watch ( CALL :watch & GOTO end )
+IF /I %choice%==lint ( CALL :lint & GOTO end )
+IF /I %choice%==map ( CALL :map & GOTO end )
+IF /I %choice%==test ( CALL :test & GOTO end )
+IF /I %choice%==diagram ( CALL :diagram & GOTO end )
+IF /I %choice%==doc ( CALL :doc & GOTO end )
+IF /I %choice%==all ( CALL :all & GOTO end )
 GOTO INVALID
 
 
 ::Invalid task
 :INVALID
-echo.
+echo/
 echo Invalid Task.
-IF NOT DEFINED %didinput% (
-echo Valid Tasks are:
-echo    clean, lua, watch, lint, map, test, diagram, doc
-echo.
+IF NOT DEFINED didinput (
+	echo Valid Tasks are:
+	echo    %tasks%
+	echo/
 )
-PAUSE
-GOTO :EOF
+GOTO end
 
 
 
 ::Makefile commands
 :clean
 echo "clean"
-PAUSE
 GOTO :EOF
 
 :lua
 echo "lua"
-PAUSE
 GOTO :EOF
 
 :watch
 echo "watch"
-PAUSE
 GOTO :EOF
 
 :lint
 echo "lint"
-PAUSE
 GOTO :EOF
 
 :map
 echo "map"
-PAUSE
 GOTO :EOF
 
 :test
 echo "test"
-PAUSE
 GOTO :EOF
 
 :diagram
 echo "diagram"
-PAUSE
 GOTO :EOF
 
 :doc
 echo "doc"
-PAUSE
 GOTO :EOF
 
+:all
+CALL :lua
+CALL :map
+CALL :doc
+GOTO :EOF
+
+:end
+PAUSE
